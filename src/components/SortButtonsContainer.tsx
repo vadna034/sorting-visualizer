0b4c054571916f58d59a4368358ab88260919867
Shane Vadnais <shanevadnais@outlook.com>
2025-02-18 13:50:11 -0600

    Make buttons into a group

--- src/components/SortButtonsContainer.tsx
+++ src/components/SortButtonsContainer.tsx
@@ -1,4 +1,4 @@
-import { Button } from '@mui/material';
+import { Button, ButtonGroup } from '@mui/material';
 import React from 'react';
 
 export interface SortButtonsContainerProps {
@@ -7,15 +7,17 @@ export interface SortButtonsContainerProps {
 
 export function SortButtonsContainer(props: SortButtonsContainerProps) {
     return <>
-        <Button>Bubble Sort</Button>
-        <Button>Selection Sort</Button>
-        <Button>Insertion Sort</Button>
-        <Button>Merge Sort</Button>
-        <Button>Quick Sort</Button>
-        <Button>Heap Sort</Button>
-        <Button>Cycle Sort</Button>
-        <Button>Tree Sort</Button>
-        <Button>Shell Sort</Button>
-        <Button>Radix Sort</Button>
+        <ButtonGroup>
+            <Button>Bubble Sort</Button>
+            <Button>Selection Sort</Button>
+            <Button>Insertion Sort</Button>
+            <Button>Merge Sort</Button>
+            <Button>Quick Sort</Button>
+            <Button>Heap Sort</Button>
+            <Button>Cycle Sort</Button>
+            <Button>Tree Sort</Button>
+            <Button>Shell Sort</Button>
+            <Button>Radix Sort</Button>
+        </ButtonGroup>
     </>
 }
